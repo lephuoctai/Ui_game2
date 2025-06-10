29f87944b564292d8fe5d95a9735f9e7e25b713f
TaiLe <lephuoctai2006@gmail.com>
2025-06-11 00:18:32 +0700

    backup - screen 2 (50%)

--- app/src/main/java/com/todo/ui_game2/signup_page.java
+++ app/src/main/java/com/todo/ui_game2/signup_page.java
@@ -1,9 +1,6 @@
 package com.todo.ui_game2;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class signup_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        ImageButton btn_login = findViewById(R.id.btn_login);
-        ImageButton btn_signup = findViewById(R.id.btn_signup);
-        Button btn_signup_with_google = findViewById(R.id.btn_signup_with_google);
-        Button btn_continue_as_guest = findViewById(R.id.btn_continue_as_guest);
-
-        btn_signup.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, signup_page.class);
-            startActivity(intent);
-        });
-
     }
 }
