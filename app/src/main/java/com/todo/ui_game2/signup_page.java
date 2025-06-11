fd19510c66a2507394b9088ba3cd44bafa050d7b
TaiLe <lephuoctai2006@gmail.com>
2025-06-11 23:30:57 +0700

    backup 4 - screen 2 (100%)

--- app/src/main/java/com/todo/ui_game2/signup_page.java
+++ app/src/main/java/com/todo/ui_game2/signup_page.java
@@ -1,14 +1,34 @@
 package com.todo.ui_game2;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class signup_page extends AppCompatActivity {
+    public void paintText(TextView view, String src, ArrayList<String> text){
+        SpannableString spannableString = new SpannableString(src);
+        int color = ContextCompat.getColor(this, R.color.access);
+
+        for(String t : text){
+            int start = src.indexOf(t);
+            int end = start + t.length();
+
+            spannableString.setSpan(new ForegroundColorSpan(color), start, end, spannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+
+        view.setText(spannableString);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +37,23 @@ public class signup_page extends AppCompatActivity {
         setContentView(R.layout.activity_signup_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        String src = "By clicking continue, you agree to our \nTerms of Service  and  Privacy Policy.";
+        TextView terms = findViewById(R.id.terms);
+        ArrayList<String> text = new ArrayList<>();
+        text.add("Terms of Service");
+        text.add("Privacy Policy");
+
+        paintText(terms, src, text);
+
+        String src2 = "Already have an account? Log in >";
+        TextView login_have_account = findViewById(R.id.login_have_account);
+        ArrayList<String> textLogin = new ArrayList<>();
+        text.add("Log in >");
+
+        paintText(login_have_account, src2, textLogin);
+
     }
 }
